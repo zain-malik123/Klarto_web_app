0d2f3af405230eaa56432f66d20e3f8219b73e71
Syed Rehan Ahmed Nasim <rehannasim0@gmail.com>
2026-02-21 01:37:05 +0500

    new api filter

--- web/env.js
+++ web/env.js
@@ -1,3 +1,7 @@
 // Generated from .env (GOOGLE_CLIENT_ID)
 window.__env = window.__env || {};
 window.__env.GOOGLE_CLIENT_ID = "577898674586-5g9duhiros1nl22d0hcc3earmgco7b0p.apps.googleusercontent.com";
+// Debug: print to browser console so we can confirm runtime value
+try {
+	console.log('[env.js] GOOGLE_CLIENT_ID=', window.__env.GOOGLE_CLIENT_ID);
+} catch (e) {}
