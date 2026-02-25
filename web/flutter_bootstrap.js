12769947b70d7f8f910dbf3cb648943a583a639a
Syed Rehan Ahmed Nasim <rehannasim0@gmail.com>
2026-02-25 21:34:19 +0500

    new api filter

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1393057343"
+    serviceWorkerVersion: "3591908677"
   }
 });
